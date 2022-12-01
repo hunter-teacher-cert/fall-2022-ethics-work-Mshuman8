8a8443e52d25b46c9a067aeeb0af3a90cbebfbd4
Marisa Shuman <37717052+Mshuman8@users.noreply.github.com>
2022-12-01 12:04:56 +0000

    Final project paper, slides, code

--- script.js
+++ script.js
@@ -113,68 +113,6 @@
 
 
     
-
-
-//SAMPLE
-
-// // More API functions here:
-//     // https://github.com/googlecreativelab/teachablemachine-community/tree/master/libraries/image
-
-//     // the link to your model provided by Teachable Machine export panel
-//     const URL = "https://teachablemachine.withgoogle.com/models/qS04kbq4K/";
-
-//     let model, webcam, labelContainer, maxPredictions;
-//     let image1 = document.querySelector("#aziz")
-
-//     // Load the image model and setup the webcam
-//     async function init() {
-//         const modelURL = URL + "model.json";
-//         const metadataURL = URL + "metadata.json";
-
-//         // load the model and metadata
-//         // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
-//         // or files from your local hard drive
-//         // Note: the pose library adds "tmImage" object to your window (window.tmImage)
-//         model = await tmImage.load(modelURL, metadataURL);
-//         maxPredictions = model.getTotalClasses();
-
-//         // Convenience function to setup a webcam
-//         const flip = true; // whether to flip the webcam
-//         webcam = new tmImage.Webcam(200, 200, flip); // width, height, flip
-//         await webcam.setup(); // request access to the webcam
-//         await webcam.play();
-//         window.requestAnimationFrame(loop);
-
-//         // append elements to the DOM
-//         document.getElementById("webcam-container").appendChild(webcam.canvas);
-//         labelContainer = document.getElementById("label-container");
-//         for (let i = 0; i < maxPredictions; i++) { // and class labels
-//             labelContainer.appendChild(document.createElement("div"));
-//         }
-//     }
-
-//     async function loop() {
-//         webcam.update(); // update the webcam frame
-//         await predict();
-//         window.requestAnimationFrame(loop);
-//     }
-
-//     // run the webcam image through the image model
-//     async function predict() {
-//         // predict can take in an image, video or canvas html element
-//       // console.log(webcam.canvas)
-//         // const prediction = await model.predict(webcam.canvas);
-//         const prediction = await model.predict(image1);
-//         for (let i = 0; i < maxPredictions; i++) {
-//             const classPrediction =
-//                 prediction[i].className + ": " + prediction[i].probability.toFixed(2);
-//             labelContainer.childNodes[i].innerHTML = classPrediction;
-//         }
-//     }
-
-
-// // Make an array of images -- have a separate button that randomizes the image, then it should rerun based on the current image.
-
 serenaButton = document.getElementById("serena-btn")
 serenaResultsB = document.getElementById("serena-resultsB")
 serenaButton.onclick = function(){
